ef6cb442318508afb92f865c540f47bdf2ea59b5
mj <mj@TeamCity>
2018-08-19 13:01:17 -0400

    TeamCity change in 'Base Project' project: 'First build config' build configuration was attached to 'Build and Test Django Project' template

--- .teamcity/BaseProject/buildTypes/BaseProject_FirstBuildConfig.kt
+++ .teamcity/BaseProject/buildTypes/BaseProject_FirstBuildConfig.kt
@@ -4,6 +4,7 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.*
 import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.script
 
 object BaseProject_FirstBuildConfig : BuildType({
+    template = "BuildAndTestDjangoProject"
     uuid = "f5510e50-5bf9-467c-9179-0df09bd7e557"
     id = "BaseProject_FirstBuildConfig"
     name = "First build config"
@@ -14,6 +15,7 @@ object BaseProject_FirstBuildConfig : BuildType({
     steps {
         script {
             name = "Step 1"
+            id = "RUNNER_1"
             scriptContent = """
                 echo Build config 1 step 1
                 date > date.txt
@@ -21,7 +23,9 @@ object BaseProject_FirstBuildConfig : BuildType({
         }
         script {
             name = "Step 2"
+            id = "RUNNER_2"
             scriptContent = "echo Build config 1 step 2"
         }
+        stepsOrder = arrayListOf("RUNNER_1", "RUNNER_2", "RUNNER_5", "RUNNER_6")
     }
 })
